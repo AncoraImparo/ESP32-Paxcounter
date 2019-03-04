4b857631489a2a47d0a23aae7a97181e4aab8987
sicarii-13 <jjungschlager@gmail.com>
2019-03-04 16:52:48 +0100

    updates that are Netherlands specefic.

--- src/lmic_config.h
+++ src/lmic_config.h
@@ -11,7 +11,7 @@
 
 #define CFG_eu868 1
 //#define CFG_us915 1
-//#define CFG_in866 1
+#define CFG_in866 1
 //#define CFG_au921 1
 //#define CFG_as923 1
 //#define LMIC_COUNTRY_CODE LMIC_COUNTRY_CODE_JP	/* for as923-JP */
