e598f95b4753fd9c0af8e7be29078bf6d52ba191
sicarii-13 <jjungschlager@gmail.com>
2019-03-15 14:21:35 +0100

    get mac script for pycom added

--- getmac_pycom.py
+++ getmac_pycom.py
@@ -2,6 +2,6 @@ from network import LoRa
 import ubinascii
 
 lora = LoRa(mode=LoRa.LORAWAN)
-print(ubinascii.hexlify(lora.mac()).upper().decode('utf-8'))ls
+print(ubinascii.hexlify(lora.mac()).upper().decode('utf-8'))
 
 #thismoduledoesn't work with the OS it works the pycom console 
